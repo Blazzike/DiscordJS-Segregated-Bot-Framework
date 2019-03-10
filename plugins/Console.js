9ff942a699a55d637da0acafc7139d2d37288e05
James Blazzike <blazzike@gmail.com>
2019-03-10 13:04:38 +0000

    Fix duplicate console calls

--- plugins/Console.js
+++ plugins/Console.js
@@ -1,43 +1,50 @@
 const terminal = require('readline').createInterface(process.stdin, process.stdout);
 
-exports.onReady = () => {
-  terminal.setPrompt('>');
-  terminal.on('line', str => {
-    let args = str.split(/\s+/g);
-    let label = args[0];
+const lineHandler = str => {
+  let args = str.split(/\s+/g);
+  let label = args[0];
 
-    args.shift();
+  args.shift();
 
-    let responded = false;
-    Object.values(plugins).forEach(plugin => {
-      if (!plugin.hasOwnProperty('console') || !plugin.console.hasOwnProperty('commands'))
+  let responded = false;
+  Object.values(plugins).forEach(plugin => {
+    if (!plugin.hasOwnProperty('console') || !plugin.console.hasOwnProperty('commands'))
+      return;
+
+    Object.keys(plugin.console.commands).forEach(clabel => {
+      let cmd = plugin.console.commands[clabel];
+      let labels = [clabel];
+
+      if (cmd.hasOwnProperty('aliases'))
+        labels.push(...cmd.aliases);
+
+      if (!labels.includes(label))
         return;
 
-      Object.keys(plugin.console.commands).forEach(clabel => {
-        let cmd = plugin.console.commands[clabel];
-        let labels = [clabel];
+      responded = true;
 
-        if (cmd.hasOwnProperty('aliases'))
-          labels.push(...cmd.aliases);
+      let response = cmd.exec(label, args);
+      if (typeof response === 'undefined' || response.length === 0)
+        return;
 
-        if (!labels.includes(label))
-          return;
-
-        responded = true;
-
-        let response = cmd.exec(label, args);
-        if (typeof response === 'undefined' || response.length === 0)
-          return;
-
-        console.log(response);
-      });
+      console.log(response);
     });
+  });
 
-    if (!responded)
-      console.error('Unknown command!');
-  }).on('close', () => {
+  if (!responded)
+    console.error('Unknown command!');
+};
+
+exports.onReady = () => {
+  terminal.setPrompt('>');
+
+  terminal.on('line', lineHandler).on('close', () => {
     process.exit(0);
   });
 
   terminal.prompt();
+};
+
+exports.onDisable = () => {
+  terminal.off('line', lineHandler);
 };
